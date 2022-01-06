8b492a0bbe354977cc18002080d7e3a9ce478fb6
Shannon Nolan <shannonnolan17@gmail.com>
2022-01-06 13:37:09 -0500

    More component clean up and add Read Me

--- trello/src/App.test.js
+++ trello/src/App.test.js
@@ -1,10 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
 import Board from './components/Board'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const board = screen
-  console.log(board)
-  expect(linkElement).toBeInTheDocument();
+  render(<Board />);
+  const board = screen.getByText(/To Do/i);
+  expect(board).toBeInTheDocument();
 });
--- trello/src/components/Board.js
+++ trello/src/components/Board.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types, react/require-default-props */
 import React, { useState } from 'react';
 
 const Board = () => {
@@ -7,19 +8,18 @@ const Board = () => {
   const [tasks, setTasks] = useState([]);
   const [newTask, setCreateTask] = useState('');
 
-  const handleChange = (event) => {
-    setCreateTask(event.target.value);
-  };
-
-  const handleSubmit = (event, col, position) => {
-    console.log(event)
+  const handleSubmit = (event, col) => {
     if (event.target.value === '') {
       return alert('Please enter a title for this card')
     }
+    console.log(tasks, '8')
+    console.log(newTask, 'newTask')
+    console.log(col, 'col')
 
     setNewCard(false);
-    setTasks([...tasks, { name: newTask, column: col, position }]);
+    setTasks([...tasks, { name: newTask, column: col }]);
     setCreateTask('');
+    console.log(tasks, 'lakdjfa')
     return tasks;
   };
 
@@ -37,9 +37,8 @@ const Board = () => {
     event.preventDefault();
   };
 
-  const onDrop = (ev, col) => {
-    console.log(ev, 'ev ')
-    const id = JSON.parse(ev.dataTransfer.getData("id"));
+  const onDrop = (event, col) => {
+    const id = JSON.parse(event.dataTransfer.getData("id"));
     const oldTask = tasks.find(element => element.name === id.name)
 
     if (id.column === col) {
@@ -53,6 +52,7 @@ const Board = () => {
   
     setTasks([...tasks, newTask]);
   };
+  console.log(tasks.length > 0 , 'tasks.length > 0 ')
 
   return (
     <div className='container'>
@@ -68,27 +68,27 @@ const Board = () => {
                 {col}
               </div>
               <div>
-                {tasks.length > 0 && tasks.map((task) => {
-                  if(task.column === col) {
-                    return (
-                      <div draggable onDragStart={(e) => { dragStartHandler(e, task) }} onDrop={e => { onDrop(e, col) }} className='card-container'> 
-                        {task.name} 
-                      </div> 
-                    )
-                  }
-                })}
+                {tasks.length > 0 && tasks.map((task) => (
+                  <Task
+                    key={task}
+                    task={task}
+                    dragStartHandler={dragStartHandler}
+                    col={col}
+                    onDrop={onDrop}
+                  />
+                ))}
               </div>
               <div>
                 {newCard && currentCol === col &&
-                  <div className='input-container'>
-                    <textarea className='textarea' name="textValue" rows="3" placeholder="Enter a title for this card..." onChange={e => handleChange(e)} />
-                    <div className='button-container'>
-                      <div className='button' onClick={(e) => handleSubmit(e, col, columns.indexOf(col))}>
-                        Add Card
-                      </div>
-                      <div className='toggle' onClick={() => toggleCard(false, col)}>X</div>
-                    </div>
-                  </div>
+                  <NewTask 
+                    toggleCard={toggleCard}
+                    handleSubmit={handleSubmit}
+                    col={col}
+                    columns={columns}
+                    newCard={newCard}
+                    currentCol={currentCol}
+                    setCreateTask={setCreateTask}
+                  />
                 }
               </div>
               <div className='new-card-button' onClick={() => toggleCard(true, col)}>
@@ -96,6 +96,48 @@ const Board = () => {
               </div>
             </div>)
           })}
+      </div>
+    </div>
+  )
+};
+
+const Task = ({task, dragStartHandler, col, onDrop}) => {
+  console.log(task.column, 'col')
+  console.log(col, 'cjdjdol')
+  if (task.column === col) {
+    return(
+      <div 
+        draggable
+        onDragStart={(e) => { dragStartHandler(e, task) }}
+        onDrop={(e) => { onDrop(e, col) }}
+        className='card-container'> 
+        {task.name} 
+      </div> 
+    )
+  }
+  return null;
+};
+
+const NewTask = ({toggleCard, handleSubmit, col, columns, setCreateTask}) => {
+  const handleChange = (event) => {
+    setCreateTask(event.target.value);
+  };
+
+  return(
+    <div className='input-container'>
+      <textarea 
+        className="textarea"
+        name="textValue"
+        rows="3"
+        placeholder="Enter a title for this card..."
+        onChange={e => handleChange(e)} />
+      <div className='button-container'>
+        <div 
+          className='button'
+          onClick={(e) => handleSubmit(e, col, columns.indexOf(col))}>
+          Add Card
+        </div>
+        <div className='toggle' onClick={() => toggleCard(false, col)}>X</div>
       </div>
     </div>
   )
